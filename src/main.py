c61481425ac3889cf5fbaf4a503b926ba40e633e
openamiguel <openamiguel@gmail.com>
2018-08-04 20:28:29 -0700

    Create main.py
    first upload for the main code

---
+++ src/main.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 4, 15:24:00, 2018
+
+Parses the online Tethys database for paper titles.
+
+In my code, a tag is defined as a broad research category (e.g. stressors), and
+a subtag is defined a more specific research category (e.g. chemicals as 
+stressors).
+
+The page number in Tethys' online information follows an interesting format. 
+Page 1 has no suffix, page 2 has the suffix "?page=1", and so on, with page N 
+having suffix "?page={}".format(N - 1). 
+
+@author: openamiguel
+"""
+
+from bs4 import BeautifulSoup
+import logging
+import os
+import pandas as pd
+import re
+import sys
+import time
+import urllib.error
+import urllib.request
+
+# Up-to-date as of August 4, 2018
+DELIM = "\t"
+TABLE_COLUMNS = ['title', 'authors', 'date', 'content_type', 'technology_type', 'stressor', 'receptor']
+TETHYS_URL = "https://tethys.pnnl.gov{}{}{}"
+TETHYS_TAG_SUBTAG = {
+			'stressor': ['chemicals', 'dynamic-device', 'emf', 'energy-removal', 
+					'lighting', 'noise', 'static-device'], 
+			'receptor': ['bats', 'benthic-invertebrates', 'birds', 'ground-nesting-birds', 
+					'passerines', 'raptors', 'seabirds', 'shorebirds', 'waterfowl', 
+					'ecosystem', 'fish', 'marine-mammals', 'sea-turtles', 
+					'terrestrial-mammals', 'farfield-environment', 'nearfield-habitat', 
+					'socio-economics', 'aesthetics', 'climate-change', 'fishing', 
+					'legal-and-policy', 'navigation', 'recreation', 'stakeholder-engagement'], 
+			'technology-type': ['marine-energy-general', 'riverine', 'ocean-current', 
+					'otec', 'salinity-gradient', 'tidal', 'wave', 'wind-energy-general', 
+					'land-based-wind', 'offshore-wind'], 
+			'interactions': ['attraction', 'avoidance', 'changes-sediment-transport', 
+					'changes-water-quality', 'collisionevasion', 'entrapment']
+			}
+
+def scrape_all_papers(logger, fpath):
+	""" Iterates through all tags and subtags to get the papers stored on Tethys.
+		Inputs: folderpath to write files to
+		Outputs: none (all outputs written to files)
+	"""
+	# Iterates through all tags and subtags
+	for tag in TETHYS_TAG_SUBTAG:
+		tag_df = pd.DataFrame(columns=TABLE_COLUMNS)
+		for subtag in TETHYS_TAG_SUBTAG[tag]:
+			subtag_df = pd.DataFrame(columns=TABLE_COLUMNS)
+			# Gets data from all pages
+			pagenum = 0
+			while True:
+				page_df = scrape_page(logger, tag, subtag, pagenum=pagenum)
+				if page_df is None:
+					break
+				# Adds the dataframe from this page to the subtag dataframe
+				subtag_df = pd.concat([subtag_df, page_df], sort=False)
+				pagenum += 1
+			# Writes the subtag dataframe to a file
+			subtag_df.to_csv("{}{}-{}.csv".format(fpath, tag, subtag), sep=DELIM, index=False)
+			# Adds the subtag dataframe to the tag dataframe
+			tag_df = pd.concat([tag_df, subtag_df], sort=False)
+		# Writes the subtag dataframe to a file
+		tag_df.to_csv("{}{}.csv".format(fpath, tag), sep=DELIM, index=False)
+	return True
+
+def scrape_page(logger, tag, subtag, pagenum=""):
+	""" Gets the table data from a given tag, subtag and page number
+		Reads a Tethys table in the same order as the other code, ensuring that
+			the output is consistent. 
+		Inputs: tag, subtag, and page number
+		Outputs: dataframe of paper data from given URL
+	"""
+	# Gets the Tethys URL based on tag and subtag
+	pagenum_suffix = "?page={}".format(pagenum) if pagenum > 0 else ""
+	tag_subtag_url = TETHYS_URL.format("/" + tag, "/" + subtag, pagenum_suffix)
+	# Tries to read a table from the website
+	# Handles several kinds of errors
+	data = None
+	try: 
+		data = pd.read_html(tag_subtag_url, match="Title")
+	except urllib.error.HTTPError as e:
+		logger.error(e)
+		logger.error("This error found at URL {}".format(tag_subtag_url))
+		return None
+	except ValueError as e:
+		logger.error(e)
+		if "No tables found" in str(e):
+			# A more meaningful error message than one from ValueError alone
+			logger.error("URL {} does not have a table of papers".format(tag_subtag_url))
+		return None
+	# Warns the user that multiple tables were matched
+	if len(data) > 1:
+		logger.warning("WARNING: multiple tables at {} that might contain paper data".format(tag_subtag_url))
+	# Assumes that the first/only match is the one with the paper data
+	page_df = data[0]
+	# Implicit check to see whether this is the right table
+	# The wrong table may have a different number of columns
+	try:
+		page_df.columns = TABLE_COLUMNS
+	except ValueError as e:
+		logger.error(e)
+		# A more meaningful error message than one from ValueError alone
+		if "Length mismatch" in str(e):
+			logger.error("First match of table at URL {} does not contain paper data".format(tag_subtag_url))
+		return None
+	# Scrapes for the external links
+	urls = scrape_page_urls(logger, tag, subtag, pagenum=pagenum)
+	page_df = pd.concat([page_df, urls], axis=1, sort=False)
+	# Returns the dataframe of page data
+	return page_df
+
+def scrape_page_urls(logger, tag, subtag, pagenum=0):
+	""" Gets a list of URLs from a given tag, subtag and page number
+		Reads a Tethys table in the same order as the other code, ensuring that
+			the output is consistent. 
+		Inputs: tag, subtag, and page number
+		Outputs: list of all paper/report URLs to get from Tethys website
+	"""
+	# Builds an empty list to store the outputs
+	pub_link_list = []
+	# Formats the given link around the standard Tethys format
+	pagenum_suffix = "?page={}".format(pagenum) if pagenum > 0 else ""
+	page_url = TETHYS_URL.format("/" + tag, "/" + subtag, pagenum_suffix)
+	html = None
+	# Tries to look for the given link and handles errors accordingly
+	try:
+		html = urllib.request.urlopen(page_url).read()
+	except urllib.error.HTTPError as e:
+		logger.error(e)
+		return None
+	soup = BeautifulSoup(html, "lxml")
+	# Iterates through all <a href> tags that point to Tethys publications
+	# These links are in-house locations for Tethys papers
+	for link in soup.findAll('a', attrs={'href': re.compile("/publications/")}):
+		link_end = link.get('href')
+		# Tries to read the publication link from Tethys
+		tethys_pub_link = TETHYS_URL.format(link_end, "", "")
+		tethys_pub_html = urllib.request.urlopen(tethys_pub_link).read()
+		tethys_pub_soup = BeautifulSoup(tethys_pub_html, "lxml")
+		# Tries to get the external publication link (ex. Wiley)
+		# Default value is the original Tethys link
+		pub_link = tethys_pub_link
+		try:
+			pub_link = tethys_pub_soup.find('a', href=True, text='External Link')['href']
+		except TypeError:
+			pass
+		# Adds the link to the list of links
+		pub_link_list.append(pub_link)
+	# Convert the list to a Series and return
+	pub_link_series = pd.Series(pub_link_list, name="paper_url")
+	return pub_link_series
+
+def main():
+	""" Run the code and time the whole process """
+	# Get the command argument prompts
+	prompts = sys.argv
+	logdir = prompts[prompts.index('-logpath')+1]
+	if logdir[-1] == '/': logdir = logdir[:-1]
+	# Initialize logger
+	logger = logging.getLogger(__name__)
+	logger.setLevel(logging.DEBUG)
+	# Set file path for logger
+	handler = logging.FileHandler('{}/hyperion.log'.format(logdir))
+	handler.setLevel(logging.DEBUG)
+	# Format the logger
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	handler.setFormatter(formatter)
+	# Add the new format
+	logger.addHandler(handler)
+	# Format the console logger
+	consoleHandler = logging.StreamHandler()
+	consoleHandler.setLevel(logging.INFO)
+	consoleHandler.setFormatter(formatter)
+	# Add the new format to the logger file
+	logger.addHandler(consoleHandler)
+	# Indicate which file is running
+	logger.info("----------INITIALIZING NEW RUN OF %s----------", os.path.basename(__file__))
+	# Save the folder path
+	fpath = prompts[prompts.index('-filepath')+1]
+	if fpath[-1] != '/': fpath = fpath + "/"
+	start_time = time.time()
+	fpath = "/Users/openamiguel/Desktop/tethys/"
+	scrape_all_papers(logger, fpath)
+	end_time = time.time()
+	logger.info("Full time elapsed: {0:.6f}".format(end_time - start_time))
+
+if __name__ == "__main__":
+	main()
